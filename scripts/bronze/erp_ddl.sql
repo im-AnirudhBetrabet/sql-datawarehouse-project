1c71f38c691c534d649a6b9b108e203d60a67965
im-AnirudhBetrabet <study.datascience.anirudh@gmail.com>
2025-03-06 10:08:58 +0530

    Add bronze layer table ddl scripts

---
+++ scripts/bronze/erp_ddl.sql
@@ -0,0 +1,43 @@
+/*
+===============================================================================
+DDL Script: Create Bronze ERP Tables
+===============================================================================
+Script Purpose:
+    This script creates erp tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
+------- Creating cust az12 table -------
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_cust_az12;
+GO
+
+CREATE TABLE bronze.erp_cust_az12(
+	cid	  NVARCHAR(50),
+	dbate DATE,
+	gen   NVARCHAR(50)
+);
+
+------- Creating loc A101 table -------
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_loc_a101;
+GO
+
+CREATE TABLE bronze.erp_loc_a101(
+	cid   NVARCHAR(50),
+	cntry NVARCHAR(50)
+);
+
+------- Creating loc A101 table -------
+IF OBJECT_ID('bronze.px_cat_G1V2', 'U') IS NOT NULL
+	DROP TABLE bronze.px_cat_G1V2;
+GO
+
+CREATE TABLE bronze.px_cat_G1V2(
+	id			NVARCHAR(50),
+	cat			NVARCHAR(50),
+	sub_cat		NVARCHAR(50),
+	maintenance NVARCHAR(50)
+);
